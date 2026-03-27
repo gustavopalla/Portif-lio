4103817665cccc51509ab714d68500b2d120f598
Gustavo Palla <pallagp34@gmail.com>
2026-03-27 15:49:44 -0300

    att

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'core/theme/app_theme.dart';
 import 'features/home/presentation/pages/home_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // 1. Carregar variáveis de ambiente
-  await dotenv.load(fileName: ".env");
-
-  // 2. Inicializar Supabase
-  await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL'] ?? '',
-    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
-  );
-
   runApp(const MyApp());
 }
 
